f49d12eed5cd1ac0066482e69dbe58270d5eeb1e
Jupiter <Jupiter@DESKTOP-P9Q82F2>
2021-05-05 22:00:12 +0000

    New Update

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -12,49 +12,157 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApplication1
+ 
+
+
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int minutes;
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += Timer_Tick;
 
             SetUpGame();
+            
+
+
+
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            minutes--;
+            timeTextBlock.Text = minutes.ToString("01s");
+            if (matchesFound == 8 || minutes == 0)
+            {
+                timer.Stop();
+                timeTextBlock.Text = "Time: " + timeTextBlock.Text + "\nPlay again? 💮";
+                
+
+
+            }
+            
+            
+        }
+        
 
         private void SetUpGame()
         {
             List<string> animalEmoji = new List<string>()
             {
-                "ꔆ", "ꔆ",
-                "ꔻ", "ꔻ",
-                "ꕸ","ꕸ",
-                "ꔮ", "ꔮ",
-                "ꔶ", "ꔶ",
-                "ꖘ", "ꖘ",
-                "ꘑ", "ꘑ",
-                "ꘚ", "ꘚ",
+
+
+                "🦊", "🦊",
+                "🐱", "🐱",
+                "🐕","🐕",
+                "🦍", "🦍",
+                "🐮", "🐮",
+                "🦄", "🦄",
+                "🐷", "🐷",
+                "🐭", "🐭",
 
 
 
 
 
-            };
+
+
+
+
+        };
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Random random = new Random();
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
                 int index = random.Next(animalEmoji.Count);
                 string nextEmoji = animalEmoji[index];
                 textBlock.Text = nextEmoji;
-                // animalEmoji.RemoveAt(index);
+                animalEmoji.RemoveAt(index);
+             
+                }
+                timer.Start();
+                matchesFound = 0;
+                minutes = 150;
+               
+
+               
 
             }
 
+        }
+        TextBlock lastClickedTextBlock;
+        bool isMatch = false;
+
+        private void TextBlock_mouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            
+            if (isMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastClickedTextBlock = textBlock;
+                isMatch = true;
+            } 
+            else if (textBlock.Text == lastClickedTextBlock.Text )
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                isMatch = false;
+                
+            }
+           
+            else
+            {
+                lastClickedTextBlock.Visibility = Visibility.Visible;
+                isMatch = false;
+            }
+
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8 )
+            {
+
+
+                SetUpGame();
+
+
+            }
+             
         }
     }
 }
